8af86967c635c0beec307f922e281e84d7bec293
kfucGG <zaharnagibator1337@gmail.com>
2023-01-15 15:36:30 +0300

    rm

--- src/main/java/ru/kata/spring/boot_security/demo/repository/UserRepository.java
+++ src/main/java/ru/kata/spring/boot_security/demo/repository/UserRepository.java
@@ -3,6 +3,7 @@ package ru.kata.spring.boot_security.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.entity.User;
 
@@ -10,7 +11,6 @@ import java.util.Optional;
 
 
 @Repository
-@Transactional(readOnly = true)
 public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query("select u from User u join fetch u.userRoles where u.name = :name")
